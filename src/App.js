1fc72c1e0e631df9bfd32b863b796d63bf4d3c75
Cesar GJ <cesar@gutierrezjaime.cat>
2021-03-01 17:13:16 +0100

    Level2  Estructura de styled-components

--- src/App.js
+++ src/App.js
@@ -1,36 +1,15 @@
 import React, { Component } from 'react'
-import styled from 'styled-components'
 
 //Crear un id por cada elemento del array
 import { generate as id  } from "shortid";
 
 //Componentes
-import Linea from './components/Linea/Linea'
+import Linea from './components/linea'
 
 //StyledComponents 
-const StyledBody = styled.body `
-  height: 100vh;
-  background-image: url("http://sertorio.synology.me/images/romeo-julieta.svg");
-  background-position: center center;
-  background-repeat: no-repeat;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  `
+import { GlobalStyle, StyledBox } from './application/GlobalStyles'
 
-const StyledBox = styled.section `
-  width: 20rem;
-  height: 30rem;
-  background-color: #282c34;
-  opacity: 0.8;
-  -webkit-box-shadow: 5px 7px 19px 16px rgba(0,0,0,0.84); 
-  box-shadow: 5px 7px 19px 16px rgba(0,0,0,0.84);
-  overflow: hidden;
-  display:flex;
-  flex-direction: column;
-  justify-content: space-between;
-`
+
 
 //Componente de clase
 class App extends Component {
@@ -48,13 +27,14 @@ class App extends Component {
   render() {
     const obraTeatro = this.state.text 
     return (
-      <StyledBody>
-        <StyledBox>
-            {obraTeatro.map(frase => (
-                <Linea fraseItem={frase} key={id()} />
-            ))}
-        </StyledBox>
-      </StyledBody>
+      <>
+        <GlobalStyle />
+          <StyledBox>
+              {obraTeatro.map(frase => (
+                  <Linea fraseItem={frase} key={id()} />
+              ))}
+          </StyledBox>
+      </>
     );
   }
 }
--- src/components/Linea/Linea.jsx
+++ src/components/Linea/Linea.jsx
@@ -1,10 +1,4 @@
-import styled from 'styled-components'
-
-const StyledLinea = styled.h3 `
-  background-color: #282c34;
-   color: white;
-   text-align: center;
-  `
+import {StyledLinea} from './Linea.styled'
 
 const Linea = (props) => {
     return (
